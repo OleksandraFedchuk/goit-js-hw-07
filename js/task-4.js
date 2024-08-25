429d8a73c44b61211879de58b1a3037f9f327746
Oleksandra <sunnyem@seznam.cz>
2024-08-25 21:15:45 +0200

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,9 +1,26 @@
 const form = document.querySelector('.login-form');
-console.log(form);
 
 form.addEventListener('submit' , submitHandler);
 
 function submitHandler(event){
-console.log(event.currentTarget.value);
+event.preventDefault();
+// console.dir(event.currentTarget);
+
+const {email , password} = event.currentTarget.elements;
+// console.log(email.value);
+// console.log(password.value);
+
+const data = {
+email : email.value.trim(),
+password: password.value.trim()
+}
+console.log(data);
+
+if (email.value === '' || password.value === ''){
+   alert(`All form fields must be filled in`);
+}
+
+event.currentTarget.reset();
+
 }
 
